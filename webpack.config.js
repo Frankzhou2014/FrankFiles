36dc29097b3662eb5d322bc61be929e53942b1f6
Frankzhou2014 <shuai.zhou@innocellence.com>
2022-11-08 10:15:22 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const slsw = require("serverless-webpack");
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  entry: slsw.lib.entries,
+  target: "node",
+  output: {
+    libraryTarget: "commonjs",
+    path: path.resolve(__dirname, ".webpack"),
+    filename: "index.js",
+  },
+  resolve: {
+    alias: {
+      xlsx: path.resolve(__dirname, "./node_modules/xlsx/xlsx.js"),
+    },
+  },
+};
