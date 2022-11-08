8a12b97461cdbaf80e33d4858b0e9d13a21c9082
Frankzhou2014 <shuai.zhou@innocellence.com>
2022-11-08 10:17:14 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,24 @@
-const path = require("path");
-const slsw = require("serverless-webpack");
+GMA China Medical Data and Report OntoHHIE
+While field medical organize a local meeting, they will be required to record & request the meeting via SFDC eEvent system and following compliance, meeting service & finance process. And after meeting , field medical will also be required to record the similar info in SFDC medical CRM for reporting purpose.  But most of information in eEvent are high overlap withing CRM .
+Project Value
+The project provides a managed data hosting for long-term GMA China data management and analytics. 
+Eliminate 5 days per month waiting for IT getting required data ready. 
+Save Field Medical approx.200 hours per year on entering meeting records in Medical CRM.
+Save approx.160 hours per year IT/Business efforts on report adjustment per change/needs
+Ensure GMA China data quality, improve overall reporting availability and increase business flexibility and user friendly 
 
-module.exports = {
-  mode: slsw.lib.webpack.isLocal ? "development" : "production",
-  entry: slsw.lib.entries,
-  target: "node",
-  output: {
-    libraryTarget: "commonjs",
-    path: path.resolve(__dirname, ".webpack"),
-    filename: "index.js",
-  },
-  resolve: {
-    alias: {
-      xlsx: path.resolve(__dirname, "./node_modules/xlsx/xlsx.js"),
-    },
-  },
-};
+
+Objectives
+Backend:
+Optimize the flow of visit related data and  eEvent PR related data to reduce the impact with Commercial data. Onboard enterprise WeChat data, automated enterprise WeChat related data stream
+
+Frontend:
+Implement reporting in Qlik Sense, including of three standard reports and four self-service reports, the business that is covered active data, inquiry data, content data and WeChat data.
+Filed Medical value report 
+Filed Medical KPI Report
+MIR Standard Report
+Filed Medical Self-Service Report
+MIR Self-Service Report
+Content Activity Self-Service Report
+YEYX Self-Service Report
+
